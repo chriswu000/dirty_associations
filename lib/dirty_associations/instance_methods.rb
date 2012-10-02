cdd5627e795482e6ebf538b2423b848c3bfe0518
Chris Wu <chriswu222@gmail.com>
2012-10-02 17:05:29 -0400

    took away block, moved clear association to top of enable_dirty_associations method.  added associations_changed, is_collection?, is_singular?

--- lib/dirty_associations/instance_methods.rb
+++ lib/dirty_associations/instance_methods.rb
@@ -4,8 +4,12 @@ module DirtyAssociations
 
      # Called on an instance of the model whose associations we're interested in.  Inside the block,
      # any changes to the associations are tracked. After the block is executed, the associations are reset.
-     def enable_dirty_associations(&block)
-       raise ArgumentError, 'Must be called with a block!' unless block_given?
+     def enable_dirty_associations
+       # (&block)
+       # raise ArgumentError, 'Must be called with a block!' unless block_given?
+       
+       # Make sure to clear out before starting
+       clear_association_changes
        
        # If the user passes ":all", scan the model and set tracking on all the associations
        # That is unless there is an "all" assocition defined, in which case, continue without setting all assocations
@@ -16,10 +20,10 @@ module DirtyAssociations
        
        # Initialize the initial values and construct the dirty methods for each association
        initialize_dirty_associations
-       yield
+       # yield
        
        # Clear out when the block ends
-       clear_association_changes
+       # clear_association_changes
      end
 
      # Generate the methods for each association, and record the initial state of each association specified.
@@ -36,6 +40,16 @@ module DirtyAssociations
        end
      end
 
+     # CW Returns array of associations that have changed
+     def associations_changed
+       return [] if original_associations.empty?
+       arr = [] 
+       self.class.dirty_associations.each do |association|
+         arr << association.to_sym if __send__("#{association}_changed?".to_sym)
+       end
+       arr
+     end
+
      # Returns true if any of the valid associations have changed since tracking was initiated
      def associations_changed?
        return false if original_associations.empty?
@@ -44,6 +58,34 @@ module DirtyAssociations
        end
        false
      end
+
+
+
+    # CW start copied this from builder.rb
+    # Returns boolean if the given association is a collection association (has_many, habtm)
+    def is_collection?(association_name)
+      association_type(association_name) == :collection
+    end
+    
+    # Returns boolean if the given association is a singular association (has_one, belongs_to)
+    def is_singular?(association_name)
+      association_type(association_name) == :singular
+    end
+    
+    # Determines if the given association is a collection of resources or a single resource
+    def association_type(association_name)
+      type = self.class.reflect_on_association(association_name).macro
+      case
+      when [:has_many, :has_and_belongs_to_many].include?(type) then :collection
+      when [:belongs_to, :has_one].include?(type)               then :singular
+      else 
+        false
+      end
+    end
+
+    # CW end 
+
+
 
      private
 
